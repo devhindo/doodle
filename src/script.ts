d550cbcb4717baa680917e1ebcbca37dd2764fd9
hindawi <ahmedhindawi2001@gmail.com>
2023-04-05 16:40:26 +0200

    drawing not working yet

--- src/script.ts
+++ src/script.ts
@@ -3,4 +3,31 @@ const colorSelector = document.getElementById('color-selector') as HTMLInputElem
 colorSelector.addEventListener('input', () => {
     const selectedColor = colorSelector.value;
 });
+
+const canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+
+const context = canvas.getContext('2d');
+if(context) context.lineWidth = 5;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+document.addEventListener('mousemove', (event) => {
+    if(isDrawing && context) {
+        context.strokeStyle = (document.getElementById('color-selector') as HTMLInputElement).value;
+        context.beginPath();
+        context.moveTo(lastX, lastY);
+        context.lineTo(event.clientX, event.clientY);
+        context.stroke();
+        lastX = event.clientX;
+        lastY = event.clientY;
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDrawing = false;
+  });
+
 export {};
