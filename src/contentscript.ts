637d5a458587a0f909b913be3a5c26a41057c107
hindawi <ahmedhindawi2001@gmail.com>
2023-05-19 07:54:16 +0300

    removed all console.log

--- src/contentscript.ts
+++ src/contentscript.ts
@@ -1,4 +1,3 @@
-console.log("content script loaded");
 var isCanvasActive = false;
 
 var color: string;
@@ -30,7 +29,6 @@ document.addEventListener("keydown", (event: KeyboardEvent) => {
 });
 
 function terminateCanvas() {
-    console.log("terminate canvas with ctrl + [");
     canvas.remove();
 }
 
@@ -39,7 +37,6 @@ var context: CanvasRenderingContext2D | null;
 
 function initCanvas() {
     isCanvasActive = true;
-    console.log("init canvas with ctrl + [");
     canvas = document.createElement('canvas');
     canvas.id = 'canvas';
     document.body.appendChild(canvas);
@@ -78,27 +75,18 @@ var isDrawing = false;
 
 function getColor() {
     chrome.storage.sync.get(['color'], function (result) {
-        console.log('Value currently is (get)' + result.color);
         color = result.color;
-        console.log('before returning selected color');
     });
 }
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
     for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
         if(key === 'color') {
-            console.log('color changed');
             color = newValue;
         }
         if(key === 'size') {
-            console.log('size changed');
             size = newValue;
-            console.log('size is (change listner)' + size); 
         }
-        console.log(
-            `Storage key "${key}" in namespace "${namespace}" changed.`,
-            `Old value was "${oldValue}", new value is "${newValue}".`
-        );
     };
 });
 
@@ -107,21 +95,16 @@ function startDrawing(e: MouseEvent) {
     if (!isCanvasActive) return;
     isDrawing = true;
     draw(e);
-    console.log("start drawing");
 }
 
 function stopDrawing() {
     if (!isCanvasActive) return;
     isDrawing = false;
     context?.beginPath();
-    console.log("stop drawing");
 }
 
 function draw(e: MouseEvent) {
     if (!isCanvasActive || !isDrawing) return;
-    console.log('current color is ' + context!.strokeStyle);
-    console.log('current size is ' + context!.lineWidth);
-
     context?.lineTo(e.clientX, e.clientY);
     context!.strokeStyle = color;
     context!.lineWidth = size;
